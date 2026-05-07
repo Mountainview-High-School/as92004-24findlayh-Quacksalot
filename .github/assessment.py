89f4d2cc8a5200e284e8dd10f4a1a2edf0ea2aec
24findlayh-Quacksalot <24findlayh@mountainview.school.nz>
2026-05-07 12:43:33 +1200

    Update assessment.py

--- .github/assessment.py
+++ .github/assessment.py
@@ -7,7 +7,8 @@ time.sleep(1)
 Name=input ("whats your name? ")
 time.sleep(1)
 age=int(input ("Whats your age? "))
-#try fix < and >
+#try fix < and > (fixed)
+time.sleep(1)
 if age < 8:
     print("youre too young try our questionare for younger ones")
 else:
