1dbc9536c66f796e204dcbec73267a800b9faa3c
24findlayh-Quacksalot <24findlayh@mountainview.school.nz>
2026-05-19 09:38:46 +1200

    Update assessment.py

--- .github/assessment.py
+++ .github/assessment.py
@@ -1,8 +1,8 @@
-#survey 
+#survey
 import time
 number=int
 age=int
-print("welcome to my questionare.")
+print("welcome to my questionare for online safety.")
 time.sleep(1)
 Name=input ("whats your name? ")
 time.sleep(1)
@@ -15,3 +15,51 @@ if age > 13:
     print ("just a tad too old. why dont you try out our questionare for older kids")
 elif age:
     print ("shall we get started")
+time.sleep(2)
+print("You want to join an online gaming site. Which of the following information is okay for you to post online?")#Answer = A
+time.sleep(0.75)
+print("A, a nickname?")
+time.sleep(0.75)
+print("B, your name?")
+time.sleep(0.75)
+print("C, your email address?")
+time.sleep(0.75)
+answer = input ("Is it A, B, Or C? ")
+
+
+
+
+
+time.sleep(2)
+print("Someone sends you a text thats is hurtful and makes you feel bad about yourself. what should you do?")#Answer = B
+time.sleep(0.75)
+print("A, Delete the message and try to forget about it?")
+time.sleep(0.75)
+print("B, Keep the text and show an adult you trust?")
+time.sleep(0.75)
+print("C, Text the person back saying something mean to them?")
+time.sleep(0.75)
+answer = input ("Is it A, B, Or C? ")
+
+
+
+
+
+time.sleep(2)
+print("Someone in your class is a real bully. Some of the other people in your class say: 'Let's get them back, and spam them with random texts.' What do you reply?")#Answer = A
+time.sleep(0.75)
+print("A, 'we shouldn't be mean to them just because they're mean to us.'?")
+time.sleep(0.75)
+print("B, 'Yeah, totally they're evil and deserve it!'?")
+time.sleep(0.75)
+print("C, 'Yes, i think that is agreat idea. Maybe they will understand what it feels like, and stop bulling us!'?")
+time.sleep(0.75)
+answer = input ("Is it A, B, Or C? ")
+
+
+
+
+
+
+
+
